aedfb7003c272eeff96dd86f26833c62c8dee1d1
“F-one-1” <fy1033409628@163.com>
2022-05-03 01:26:19 +0800

    delete server vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -32,12 +32,4 @@ export default defineConfig({
       },
     },
   },
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: 3000,
-  //   strictPort: true,
-  //   hmr: {
-  //     port: 443
-  //   }
-  // }
 })
